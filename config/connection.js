304063924d719c253fb3db52e7af888b3f6ae715
devin <devin@devins-MacBook-Pro.local>
2022-10-17 13:52:00 -0400

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {dialectOptions: {
+      ssl: {
+          rejectUnauthorized: false
+      }
+  }});
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME, 
